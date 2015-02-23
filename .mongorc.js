4f4577a7b94c652d61149325982f27d06ccd1285
Christopher A. Moore <cmoore@qti.qualcomm.com>
2015-02-23 08:38:59 -0800

    add updates

--- .mongorc.js
+++ .mongorc.js
@@ -1276,6 +1276,9 @@ function uuidToString(uuid, uuidType) {
 setVerboseShell(true);
 
 DBQuery.prototype._prettyShell = true
+DBQuery.prototype._dropall = function(){
+    console.log('DROP ALL THE DB\'s LIKE THEY HOT!');
+});
 
 DB.prototype._getExtraInfo = function(action) {
     if ( typeof _verboseShell === 'undefined' || !_verboseShell ) {
